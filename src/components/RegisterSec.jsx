c250b2a59eac21e6da6574934b7f24d322968036
Covenant Ogowale <justcovenant@outlook.com>
2024-03-29 08:50:42 +0100

    register section responsiveness done

--- src/components/RegisterSec.jsx
+++ src/components/RegisterSec.jsx
@@ -1,6 +1,6 @@
 const RegisterSec = () => {
   return (
-    <div className="bg-gradient-to-r from-[#AE67FA] to-[#F49867] w-11/12 mx-auto mt-24 p-10 rounded-2xl flex justify-between">
+    <div className="bg-gradient-to-r from-[#AE67FA] to-[#F49867] w-11/12 mx-auto mt-24 p-10 rounded-2xl flex flex-col lg:flex-row gap-10 justify-between">
       <div>
         <p>Request Early Access to Get Started</p>
         <h1 className="text-2xl font-black">Register today & start exploring the endless possiblities.</h1>
